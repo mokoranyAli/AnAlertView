887d7f2c37b4f6f8efaa4501742953f41d9d6966
Mohamed <mohammed.korany.ali@gmail.com>
2020-01-14 02:41:00 +0200

    Initial commit

---
+++ Example/AnAlertView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AnAlertView
+//
+//  Created by mokoranyAli on 01/14/2020.
+//  Copyright (c) 2020 mokoranyAli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
